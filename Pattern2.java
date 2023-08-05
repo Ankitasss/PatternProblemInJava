bc861737a1ae4fbb5b5d38a14241629f4cfc74eb
Ankitasss <99815382+Ankitasss@users.noreply.github.com>
2023-08-05 21:13:34 +0530

    Questions On Pattern
    There is the questions on alphabet from A to Z all the pattern

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+package com.kodnest.assignment.pattern.AtoZ;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(j==1||i==1||i==5||i==3&j<=4||j==5&i!=3 ) {
+					System.out.print("*"+" ");
+				}
+				
+				else {
+					System.out.print(" "+" ");
+				}
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
